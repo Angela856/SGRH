fa8ceecbe547f9c4cb3d2ccf83f3f0eb36e5bdd4
Angela <an.anhuaman@duocuc.cl>
2026-05-12 01:49:34 -0400

    Eliminando rastro de carpeta duouc y sincronizando rutas correctas

---
+++ Disponibilidad/src/test/java/cl/duocuc/dsy1103/disponibilidad/DisponibilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.dsy1103.disponibilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisponibilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Tarifa/src/test/java/cl/duocuc/dsy1103/tarifa/TarifaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.dsy1103.tarifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
