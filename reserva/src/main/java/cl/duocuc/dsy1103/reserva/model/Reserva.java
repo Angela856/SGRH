f701273f6a17f2ea20f95d3e6d234412376fbb31
SB- Alumno <SB-Alumno@duoc.acad>
2026-06-17 16:42:12 -0400

    actualización en reserva-model

--- reserva/src/main/java/cl/duocuc/dsy1103/reserva/model/Reserva.java
+++ reserva/src/main/java/cl/duocuc/dsy1103/reserva/model/Reserva.java
@@ -16,12 +16,16 @@ public class Reserva {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(name= "id_usuario", nullable = false)
     private Long idUsuario;      
     
+    @Column(name= "id_habitacion", nullable = false)
     private Long idHabitacion;   
     
+    @Column(name= "fecha_inicio", nullable = false)
     private LocalDate fechaInicio;
     
+    @Column(name= "fecha_fin", nullable = false)
     private LocalDate fechaFin;
     
     private String estado;       // PENDIENTE, CONFIRMADO, CANCELADO
