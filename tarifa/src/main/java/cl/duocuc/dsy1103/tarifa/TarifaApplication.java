12d6d7bd6bf6059a7bd50bafa98b42a878c40f1e
Charlieisd34d <alm.gonzaleza@duocuc.cl>
2026-06-23 20:21:10 -0400

    arreglando un pequeño bug

--- tarifa/src/main/java/cl/duocuc/dsy1103/tarifa/TarifaApplication.java
+++ tarifa/src/main/java/cl/duocuc/dsy1103/tarifa/TarifaApplication.java
@@ -1,24 +1,11 @@
 package cl.duocuc.dsy1103.tarifa;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.Contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
-// CORRECCIÓN DUOC: Habilita el escaneo de clientes remotos Feign para comunicarse con otros microservicios
-@EnableFeignClients 
-// CUMPLIMIENTO SWAGGER: Define el título, descripción y versión de la API de forma centralizada
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Microservicio de Tarifas - SGRH",
-        version = "1.0.0",
-        description = "API para la administración de precios, temporadas y cotizaciones de habitaciones en el Sistema de Gestión de Recursos Hoteleros.",
-        contact = @Contact(name = "Módulo de Almendra", email = "almendra@duocuc.cl")
-    )
-)
+
 public class TarifaApplication {
 
 	public static void main(String[] args) {
