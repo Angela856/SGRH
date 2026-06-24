9f8aa19f36112de2bea0a38dabdca808edc952af
Charlieisd34d <alm.gonzaleza@duocuc.cl>
2026-06-23 22:22:27 -0400

    añadiendo puertos

--- tarifa/src/main/java/cl/duocuc/dsy1103/tarifa/Repository/TarifaRepository.java
+++ tarifa/src/main/java/cl/duocuc/dsy1103/tarifa/Repository/TarifaRepository.java
@@ -1,11 +1,9 @@
 package cl.duocuc.dsy1103.tarifa.Repository;
 import cl.duocuc.dsy1103.tarifa.Model.Tarifa;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.Optional;
-
-@Repository
 
 public interface TarifaRepository extends JpaRepository<Tarifa, Long> {
    
